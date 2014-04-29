1e57668d126bd6c2f43f479763569d512570beab
saralandset <saralandset@gmail.com>
2014-04-29 03:30:48 -0400

    populated picks fragment

--- Pickem/src/com/example/pickem/JoinPoolDialog.java
+++ Pickem/src/com/example/pickem/JoinPoolDialog.java
@@ -16,15 +16,18 @@ public class JoinPoolDialog extends DialogFragment{
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
+		
 		}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		
+		
 		return inflater.inflate(R.layout.join_pool_dialog,
                 container, false);
 		  
+		
 	}
 
 }
